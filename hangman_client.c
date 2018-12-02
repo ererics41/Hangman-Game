cc8ec6512afd34f1e8045caf134551ecfbea7ba3
Tiange Wang <wtgkevin@gmail.com>
2018-12-01 22:31:46 -0800

    Complete architecture change. TCP used with fork

--- hangman_client.c
+++ hangman_client.c
@@ -1,16 +1,16 @@
 /* 
     Tiange Wang, ID: 3717659, CS 176A Homework 5
-    Code Cited: http://www.linuxhowtos.org/data/6/client_udp.c, Sockets Tutorial, Example UDP Client.
+    Code Cited: http://www.linuxhowtos.org/data/6/client_c_tcp.c, Sockets Tutorial, Example TCP Client.
 */
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
 #include <ctype.h>
 
 void error(const char *msg)
@@ -21,34 +21,45 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-    int sock, n;
-    unsigned int length;
-    struct sockaddr_in server, from;
-    struct hostent *hp;
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
     char buffer[256];
-    if (argc != 3) { 
-        printf("Usage: server port\n");
-        exit(1);
+    if (argc < 3) {
+       fprintf(stderr,"Usage: %s hostname port\n", argv[0]);
+       exit(1);
     }
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) error("socket");
-    server.sin_family = AF_INET;
-    hp = gethostbyname(argv[1]);
-    if (hp==0) error("Unknown host");
-    bcopy((char *)hp->h_addr, 
-        (char *)&server.sin_addr,
-            hp->h_length);
-    server.sin_port = htons(atoi(argv[2]));
-    length=sizeof(struct sockaddr_in);
+
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
 
     while(1) {
         printf("Ready to start the game? (y/n): ");
         bzero(buffer,256);
         fgets(buffer,256,stdin);
         if(strlen(buffer) == 2 && buffer[0] == 'y') {
+            bzero(buffer,256);
+            buffer[0] = '0';
+            n = write(sockfd,buffer,strlen(buffer));
+            if (n < 0) error("Sendto");
             break;
         } else if(strlen(buffer) == 2 && buffer[0] == 'n') {
-            close(sock);
+            close(sockfd);
             return 0;
         } else {
             printf("Error! Please enter y or n.\n");
@@ -56,23 +67,21 @@ int main(int argc, char *argv[])
     }
 
     while(1) {
+        n = read(sockfd,buffer,255);
+        if (n < 0) error("recvfrom");
         printf("Letter to guess: ");
         bzero(buffer,256);
         fgets(buffer,256,stdin);
-        if(strlen(buffer) == 1 && isalpha(buffer[0]) != 0) {
-            printf("placeholder stuff");
-            return 0;
+        if(strlen(buffer) == 2 && isalpha(buffer[0]) != 0) {
+            buffer[1] = buffer[0];
+            buffer[0] = '1';
+            buffer[2] = '\0';
+            n = write(sockfd,buffer,strlen(buffer));
+            if (n < 0) error("Sendto");
         } else {
-            printf("Error! Please guess one letter.");
+            printf("Error! Please guess one letter.\n");
         }
     }
-    
-    n = sendto(sock,buffer,strlen(buffer),0,(const struct sockaddr *)&server,length);
-    if (n < 0) error("Sendto");
-    n = recvfrom(sock,buffer,256,0,(struct sockaddr *)&from, &length);
-    if (n < 0) error("recvfrom");
-    write(1,"Got an ack: ",12);
-    write(1,buffer,n);
-    close(sock);
+
     return 0;
 }
--- hangman_server.c
+++ hangman_server.c
@@ -1,53 +1,85 @@
 /* 
     Tiange Wang, ID: 3717659, CS 176A Homework 5
-    Code Cited: http://www.linuxhowtos.org/data/6/server_udp.c, Sockets Tutorial, Example UDP Server.
+    Code Cited: http://www.linuxhowtos.org/data/6/server_c_tcp.c, Sockets Tutorial, Example TCP Server.
 */
-#include <sys/types.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <string.h>
 #include <netdb.h>
-#include <stdio.h>
 
-void error(const char *msg)
-{
+void error(const char *msg) {
     perror(msg);
-    exit(0);
+    exit(1);
 }
 
-int main(int argc, char *argv[])
-{
-    int sock, length, n;
-    socklen_t fromlen;
-    struct sockaddr_in server;
-    struct sockaddr_in from;
-    char buf[1024];
+int main(int argc, char *argv[]) {
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
     if (argc < 2) {
-        fprintf(stderr, "ERROR, no port provided\n");
-        exit(0);
+        fprintf(stderr,"Usage: hostname port\n");
+        exit(1);
     }
 
-    sock=socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) error("Opening socket");
-    length = sizeof(server);
-    bzero(&server,length);
-    server.sin_family=AF_INET;
-    server.sin_addr.s_addr=INADDR_ANY;
-    server.sin_port=htons(atoi(argv[1]));
-    if (bind(sock,(struct sockaddr *)&server,length)<0) 
-        error("binding");
-    fromlen = sizeof(struct sockaddr_in);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR on binding");
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
 
+    int pid;
     while (1) {
-        n = recvfrom(sock,buf,1024,0,(struct sockaddr *)&from,&fromlen);
-        if (n < 0) error("recvfrom");
-        write(1,"Received a datagram: ",21);
-        write(1,buf,n);
-        n = sendto(sock,"Got your message\n",17,0,(struct sockaddr *)&from,fromlen);
-        if (n  < 0) error("sendto");
-    }
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        if (newsockfd < 0)
+        error("ERROR on accept");
+        //fork new process
+        pid = fork();
+        if (pid < 0) {
+            error("ERROR in new process creation");
+        } else if (pid == 0) {
+            //child process
+            close(sockfd);
+            char word[256] = "apple";
+            int word_len = 5;
+            while(1) {
+                bzero(buffer, 256);
+                n = read(newsockfd, buffer, 256);
+                if (n < 0) error("ERROR reading from socket");
+                if(buffer[0] == '0') {
+                    buffer[1] = word_len; // Word length
+                    buffer[2] = 0; // Num incorrect
+                    for(int i = 3; i < 3 + word_len; i++) {
+                        buffer[i] = '_';
+                    }
+                    n = write(newsockfd, buffer, 256);
+                    if (n < 0) error("ERROR writing to socket");
+                } else {
+                    for(int i = 0; i < word_len; i++) { // Change to length later
+                        if(word[i] == buffer[i + 3]) {
 
+                        }
+                    }
+                }
+            }
+            close(newsockfd);
+            exit(0);
+        } else {
+            //parent process
+            close(newsockfd);
+        }
+    }
     return 0;
 }
